09e3071f4e716adadd41052adeb5199857abe7f4
sayoung0820 <shannonashley78@gmail.com>
2022-11-23 16:56:11 -0500

    submitting what I could complete for the week 4 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -9,6 +11,12 @@
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
+
+// describe("functionName",  ( ) => { 
+// } if("describes what functionName does", ( ) => {
+// expect(functionName("pats argument through as needed )).toEqual("expected output")
+// })
+// })
 
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
@@ -21,9 +29,36 @@ const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+describe(newColors, () => {
+    it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+    expect(newColors(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+    expect(newColors(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    })
+    })
+
+    // ReferenceError: newColors is not defined
 
 // b) Create the function that makes the test pass.
 
+// Pseudocode:
+// Create a function called newColors that takes in an array, removes the first item from the array and shuffles the remaining content
+// Taking in one parameter, array
+// function will first use the .shift method to remove the first value
+// function will then use the math.random method to randomize the returning array
+// return the new array
+
+const newColors = (array) => {
+    array.shift()
+    return array.sort(() => Math.random() - 0.5)
+}
+
+// numbers.sort(function (a, b) {
+//     return Math.random() - 0.5;
+//   });
+
+// I know I am using the math.random method incorrectly here but thought I could .shift to remove the first element, then sort/randomize the remaining array but still doesn't pass
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
@@ -33,6 +68,7 @@ const votes1 = {upVotes: 13, downVotes: 2}
 // Expected output: 11
 const votes2 = {upVotes: 2, downVotes: 33}
 // Expected output: -31
+
 
 
 // b) Create the function that makes the test pass.
